12710ee2ad069f6c237b409339e4e40ebabc38f4
Champinbon <wowchik_3708@mail.ru>
2023-03-06 15:43:00 +0700

    фигуры во всех клетках

--- 1_pr.js
+++ 1_pr.js
@@ -1,10 +1,15 @@
 var divSquare = '<div id = "s$coord" class = "square $color"></div>';
 var divFigure = '<div id = "f$coord" class = "figure">$figure</div>';
+
 $(function(){
     createBoard();
-    showFigureAt(0, 'c');
-    showFigureAt(63, 'c');
+    showFigures('wwwwwwwwwwwwwwww11111111111111111111111111111111bbbbbbbbbbbbbbbb');
+    setDraggable();
 })
+
+function setDraggable(){
+    $('.figure').draggable();
+}
 
 function createBoard() {
     $('.board').html('');
@@ -20,6 +25,19 @@ function isBlackSquareAt(coord){
     return (coord % 8 + Math.floor(coord / 8)) % 2;
 }
 
+function showFigures(figure){
+    for (var coord = 0; coord < 64; coord++)
+    showFigureAt(coord, figure.charAt(coord));
+}
+
 function showFigureAt(coord, figure){
-    $('#s' + coord).html(divFigure.replace('$coord', coord).replace('$figure', figure));
+    $('#s' + coord).html(divFigure.replace('$coord', coord).replace('$figure', getCheckersSymbol(figure)));
+}
+
+function getCheckersSymbol(figure){
+    switch(figure){
+        case 'w' : return '&#128656;';
+        case 'b' : return '&#128659;';
+        default : return '';
+    }
 }
